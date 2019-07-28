a262860506951f997d7594a9782c1fb7774fe134
Borja González Seoane <garaje@glezseoane.es>
2019-07-28 13:03:54 +0200

    🍺 Add folstamp 1.1

---
+++ Formula/folstamp.rb
@@ -0,0 +1,54 @@
+class Folstamp < Formula
+  include Language::Python::Virtualenv
+
+  desc "Generate custom macOS folder icons"
+  homepage "https://github.com/GlezSeoane/folder-stamp"
+  url "https://github.com/GlezSeoane/folder-stamp/archive/v1.1.tar.gz"
+  sha256 "b1d0f020e48d57969b6a4426b9c608b55302f9870f0576df1351114e697ef7a6"
+  
+  depends_on "jpeg" => :build
+  depends_on "librsvg"
+  depends_on "python"
+
+  resource "Pillow" do
+    url "https://github.com/GlezSeoane/Pillow/archive/6.1.0.tar.gz"
+    sha256 "9a3a613e7780362605b10765274389b3e9a6fe3201dd2bceeb422d45c5c9ba18"
+  end
+  resource "filetype" do
+    url "https://files.pythonhosted.org/packages/e8/53/298887541ae479f8467d4d23e028c6d15f9811da25c582297fd3869666b7/filetype-1.0.5.tar.gz"
+    sha256 "17a3b885f19034da29640b083d767e0f13c2dcb5dcc267945c8b6e5a5a9013c7"
+  end
+    resource "monoshape" do
+    url "https://files.pythonhosted.org/packages/ac/5e/b0c25f186bde2d579ef1bb323fbd80b950c30d156f39f90b53b967dcecbb/monoshape-1.1.tar.gz"
+    sha256 "fb48231c1ce1014f7aeda3959afbcd72f0f48e016217cab628bff148061378c0"
+  end
+
+  def install
+    venv = virtualenv_create(libexec, "python3")
+
+    resource("Pillow").stage do
+      inreplace "setup.py" do |s|
+        sdkprefix = MacOS.sdk_path_if_needed ? MacOS.sdk_path : ""
+        s.gsub! "openjpeg.h", "probably_not_a_header_called_this_eh.h"
+        s.gsub! "ZLIB_ROOT = None", "ZLIB_ROOT = ('#{sdkprefix}/usr/lib', '#{sdkprefix}/usr/include')"
+        s.gsub! "JPEG_ROOT = None", "JPEG_ROOT = ('#{Formula["jpeg"].opt_prefix}/lib', '#{Formula["jpeg"].opt_prefix}/include')"
+        s.gsub! "FREETYPE_ROOT = None", "FREETYPE_ROOT = ('#{Formula["freetype"].opt_prefix}/lib', '#{Formula["freetype"].opt_prefix}/include')"
+      end
+
+      ENV.append "CFLAGS", "-I#{MacOS.sdk_path}/System/Library/Frameworks/Tk.framework/Versions/8.5/Headers" unless MacOS::CLT.installed?
+      venv.pip_install Pathname.pwd
+    end
+
+    res = resources.map(&:name).to_set - ["Pillow"]
+    res.each do |r|
+      venv.pip_install resource(r)
+    end
+
+    venv.pip_install_and_link buildpath
+    man1.install "manpages/folstamp.1"
+  end
+
+  test do
+    system "python", "-c", "'import monoshape.__main__'"
+  end
+end
--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Use `brew install glezseoane/tap/<formula>` to install a keg.
 
 Own maintained projects.
 
+- `folstamp`: Generate custom macOS folder icons. View [homepage](https://github.com/glezseoane/folder-stamp).
 - `gitcher`: The git profile switcher. View [homepage](https://github.com/glezseoane/gitcher).
 - `gitfind`: Find local git repositories. View [homepage](https://github.com/glezseoane/gitfind).
 - `monoshape`: Extracts monochromatic shapes. View [homepage](https://github.com/glezseoane/monoshape).
