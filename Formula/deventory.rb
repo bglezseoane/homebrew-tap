780883cf2e5510748e2f42c1b9484cb0da6fcf49
Borja González Seoane <garaje@glezseoane.es>
2019-10-27 11:56:29 +0100

    🍺 Add deventory 0.1dev0

---
+++ Formula/deventory.rb
@@ -0,0 +1,18 @@
+class Deventory < Formula
+  include Language::Python::Virtualenv
+
+  desc "The development project inventory manager"
+  homepage "https://github.com/glezseoane/deventory"
+  url "https://github.com/glezseoane/deventory/archive/v0.1dev0.tar.gz"
+  sha256 "d942a2b51f4fe717accffb54a759aaa673ec8c3ba0faab1dba20554d56be9639"
+  
+  depends_on "python"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "python", "-c", "'import deventory'"
+  end
+end
