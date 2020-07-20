370b4af06a59b49c621af08c69878e8e26c76350
Borja González Seoane <garaje@glezseoane.es>
2020-07-20 12:06:49 +0200

    🍻 Update fwitness to 0.4.dev6

--- Formula/fwitness.rb
+++ Formula/fwitness.rb
@@ -3,9 +3,9 @@ class Fwitness < Formula
 
   desc "Consolidate Mac OS Finder metadata between several machines"
   homepage "https://github.com/bglezseoane/fwitness"
-  url "https://files.pythonhosted.org/packages/d8/3a/dc99c1d4d8db68057f3125dd5f6b0dce3986da9250e52ba6bd87f6017779/fwitness-0.4.dev5.tar.gz"
-  sha256 "63488c8236c52b2161f5f0598a35abcf196c167e8eb6ff763ecbb43613bca3c7"
-  version "0.4.dev5"
+  url "https://files.pythonhosted.org/packages/a1/ec/a432a020690949429b3491a208b905a1fd9e2953656fb5fb713b95794de1/fwitness-0.4.dev6.tar.gz"
+  sha256 "578d7aedc187aa641d417f95cd81075d8a664213c8db0fc777c22416886d1d44"
+  version "0.4.dev6"
 
   depends_on "python@3.8"
   depends_on "tag"
