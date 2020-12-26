8dae5ad8fc3d75883d8a7132b23a81f16850d8a8
Borja González Seoane <garaje@glezseoane.es>
2020-12-26 19:09:14 +0100

    🍻 Update cpcalendars to 0.1.1

--- Formula/cpcalendars.rb
+++ Formula/cpcalendars.rb
@@ -2,8 +2,8 @@ class Cpcalendars < Formula
   desc "CLI app to copy Mac OS Calendar.app calendars sources to other location"
   homepage "https://github.com/bglezseoane/cpcalendars"
   url "https://github.com/bglezseoane/cpcalendars/releases/latest/download/cpcalendars-mac.tar.gz"
-  version "0.1.0"
-  sha256 "b20a3217bd6848404362dc6790bdbc69fcd701cb7175c29f91fb2d768639ced2"
+  version "0.1.1"
+  sha256 "a2113b6e71df8a2133f3c0f493bdaa9017824eff43d41cee68ee8519af5b7851"
 
   def install
     bin.install "cpcalendars"
