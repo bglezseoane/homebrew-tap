2298b030b4d44d5bafcae5ba18738069114fdfae
Borja González Seoane <garaje@glezseoane.es>
2020-12-30 01:01:04 +0100

    🍻 Update tmlatestbackup to 0.1.1

--- Formula/tmlatestbackup.rb
+++ Formula/tmlatestbackup.rb
@@ -2,8 +2,8 @@ class Tmlatestbackup < Formula
   desc "Really simple CLI app to obtain the date of the last Mac OS Time Machine backup"
   homepage "https://github.com/bglezseoane/tmlatestbackup"
   url "https://github.com/bglezseoane/tmlatestbackup/releases/latest/download/tmlatestbackup-mac.tar.gz"
-  version "0.1.0"
-  sha256 "a899c7812233837906cf0cf8a76970df3dfcd9ac156f0658ed55b9cb008127fe"
+  version "0.1.1"
+  sha256 "37ae5732fbe23a7448353239a4fbd42d915af3f6fee5d857c94ce9fa955f60f7"
 
   def install
     bin.install "tmlatestbackup"
